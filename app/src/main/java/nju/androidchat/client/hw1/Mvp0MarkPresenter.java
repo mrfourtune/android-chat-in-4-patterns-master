9080e9409f6dec917b9a42a945465ab60372c094
FallenMoon <825232958@qq.com>
2019-07-04 16:59:03 +0800

    hw1提交

---
+++ app/src/main/java/nju/androidchat/client/hw1/Mvp0MarkPresenter.java
@@ -0,0 +1,44 @@
+package nju.androidchat.client.mvp0;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import nju.androidchat.client.ClientMessage;
+
+@AllArgsConstructor
+public class Mvp0MarkPresenter implements Mvp0Contract.Presenter {
+
+    private Mvp0Contract.Model mvp0TalkModel;
+    private Mvp0Contract.View iMvp0TalkView;
+
+    @Getter
+    private List<ClientMessage> clientMessages;
+
+    @Override
+    public void sendMessage(String content) {
+        ClientMessage clientMessage = mvp0TalkModel.sendInformation(content);
+        refreshMessageList(clientMessage);
+    }
+
+    @Override
+    public void receiveMessage(ClientMessage clientMessage) {
+        refreshMessageList(clientMessage);
+    }
+
+    @Override
+    public String getUsername() {
+        return mvp0TalkModel.getUsername();
+    }
+
+    private void refreshMessageList(ClientMessage clientMessage) {
+        clientMessages.add(clientMessage);
+        iMvp0TalkView.showMessageList(clientMessages);
+    }
+
+
+    @Override
+    public void start() {
+
+    }
+}
